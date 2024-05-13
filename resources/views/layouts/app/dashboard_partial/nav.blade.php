abd34664bac17ac6bd78c1fbe15bcf15f14095b1
andypratama3 <andypratama1211@gmail.com>
2024-05-13 15:24:18 +0800

    feat: user, foto and url navbar in landing use done

--- resources/views/layouts/app/dashboard_partial/nav.blade.php
+++ resources/views/layouts/app/dashboard_partial/nav.blade.php
@@ -151,13 +151,13 @@
 
       <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
         <img src="{{ asset('assets_dashboard/img/profile-img.jpg')}}" alt="Profile" class="rounded-circle">
-        <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
+        {{-- <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span> --}}
       </a><!-- End Profile Iamge Icon -->
 
       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
         <li class="dropdown-header">
-          <h6>{{ Auth::user()->name }}</h6>
-          <span>{{ Auth::user()->role == 1 ? 'Admin' : 'User' }}</span>
+          {{-- <h6>{{ Auth::user()->name }}</h6>
+          <span>{{ Auth::user()->role == 1 ? 'Admin' : 'User' }}</span> --}}
         </li>
         <li>
           <hr class="dropdown-divider">
